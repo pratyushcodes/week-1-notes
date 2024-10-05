09e42a690bf0c2d98c6345780b3f513258ac133f
Pratyush <pratyushcodes>
2024-10-05 16:47:54 -0400

    overloading constructors

--- Week_1_Demos/src/Classes/Main.java
+++ Week_1_Demos/src/Classes/Main.java
@@ -2,7 +2,11 @@ package Classes;
 
 public class Main {
     public static void main(String[] args) {
-       Diceroller diceroller = new Diceroller();
-
+       /* overloaded constructors = multiple constructors within a class with the same
+        name, but have different parameters
+        name + parameter = signature
+         */
+        Pizzas myFavorite = new Pizzas("hand tossed","garlic","swiss","pepperoni");
+        Pizzas differentPizza = new Pizzas("hand tossed", "tomato", "cheddar");
     }
 }
---
+++ Week_1_Demos/src/Classes/Pizzas.java
@@ -0,0 +1,18 @@
+package Classes;
+public class Pizzas {
+    String bread;
+    String sauce;
+    String cheese;
+    String topping;
+    Pizzas(String bread, String sauce, String cheese){
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+    }
+    Pizzas(String bread, String sauce, String cheese, String topping){
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.topping = topping;
+    }
+}
